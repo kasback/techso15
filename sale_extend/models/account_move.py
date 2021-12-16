777b022f2d71d36041e59b560bf2b0dd8c27d10c
kasback <kasbajinassrallah@gmail.com>
2021-12-16 17:10:12 +0100

    Réctifs facture

--- sale_extend/models/account_move.py
+++ sale_extend/models/account_move.py
@@ -8,7 +8,14 @@ class AccountMove(models.Model):
 
     def get_footer_text(self):
         return """
-            <p class="terms" style="color: rgb(68, 75, 90);"><span style="font-weight: bolder;">Conditions de paiement</span>: Payable à la commande</p><p class="terms" style="color: rgb(68, 75, 90);"><span style="font-weight: bolder;">Mode de règlement :</span></p><p class="terms" style="color: rgb(68, 75, 90);"><span style="font-weight: bolder;">Date d'écheance :</span></p><p class="terms" style="color: rgb(68, 75, 90);"><span style="font-weight: bolder;">Coordonnées bancaires:</span>&nbsp;BMCE-BANK-CENTRE D’AFFAIRES ZENITH MILLENIUM</p><p style="color: rgb(68, 75, 90);"><span style="font-weight: bolder;"><font class="text-white">Coordonnées bancaires:&nbsp;</font></span>Compte N°&nbsp;011780000529210000129035</p><p style="color: rgb(68, 75, 90);"><span style="font-weight: bolder;"><font class="text-white">Coordonnées bancaires:&nbsp;</font></span>SWIFT N°&nbsp;BMCEMAMC</p>
+            <p class="terms" style="color: rgb(68, 75, 90);">
+                <span style="font-weight: bolder;">Conditions de paiement</span>: Payable à la Facturation
+            </p>
+            <p class="terms" style="color: rgb(68, 75, 90);"><span style="font-weight: bolder;">Mode de règlement: </span>Virement</p>
+            <p class="terms" style="color: rgb(68, 75, 90);"><span style="font-weight: bolder;">Date d'écheance: </span></p>
+            <p class="terms" style="color: rgb(68, 75, 90);"><span style="font-weight: bolder;">Coordonnées bancaires:</span>&nbsp;ATTIJARIWAFA BANK</p>
+            <p style="color: rgb(68, 75, 90);"><span style="font-weight: bolder;">Coordonnées bancaires:&nbsp;</span>Compte N°MA 64 007 780 8003004000000545 79</p>
+            <p style="color: rgb(68, 75, 90);"><span style="font-weight: bolder;">Coordonnées bancaires:&nbsp;</span>SWIFT N°&nbsp;BCMAMAMC</p>
         """
 
     footer_text = fields.Html('Informations suplémentaires', default=get_footer_text)
