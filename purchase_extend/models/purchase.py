c5af12a85b5b36c0c0c6a051e069f753180e2fd2
kasback <kasbajinassrallah@gmail.com>
2021-12-17 10:31:30 +0100

    Réctifs BC achats

--- purchase_extend/models/purchase.py
+++ purchase_extend/models/purchase.py
@@ -1,9 +1,13 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class PurchaseOrder(models.Model):
     _inherit = 'purchase.order'
 
     client_id = fields.Many2one('res.partner', 'Client')
+
+    @api.onchange('partner_id')
+    def update_supplier_ref(self):
+        self.partner_ref = self.partner_id.ref
